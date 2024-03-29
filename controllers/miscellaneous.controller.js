022ccb324a3e2a2ed8472ffdadede6870923ecdf
Mohitsen11 <mohitsen0102@gmail.com>
2024-03-29 19:34:46 +0530

    Styled the text-message

--- controllers/miscellaneous.controller.js
+++ controllers/miscellaneous.controller.js
@@ -12,7 +12,14 @@ const contactUs = async (req, res , next) => {
     try {
         
         const subject = 'Contact Us Form';
-        const textMessage = `${name} - ${email} <br /> ${message}`;
+        // const textMessage = `${name} - ${email} <br /> ${message}`;
+        const textMessage = `
+            <div style="font-family: Arial, sans-serif; background-color:#f4f4f4; padding: 20px; border-radius: 5px;">
+            <p style="margin-bottom: 10px;"><strong>Name:</strong> ${name}</p>
+            <p style="margin-bottom: 10px;"><strong>Email:</strong> ${email}</p>
+            <p><strong>Message:</strong><br />${message}</p>
+        </div>`;
+
 
         await sendEmail(process.env.CONTACT_US_EMAIL , subject , textMessage);
 
